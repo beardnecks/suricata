7e8241a63c7ee304d1dcc4b5cd09eaa61b0efb7a
Cloud User <centos@server1.novalocal>
2020-03-04 12:36:28 +0000

    added entrypoint script to dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,12 @@
 
 #TO DO
 
+# authenticate with aws cli
+aws configure set aws_access_key_id $AWS_KEY
+aws configure set aws_secret_access_key_id $AWS_SECRET_KEY
+aws configure set default_region $AWS_REGION
+
+
 ## get suricata.yml form S3
 
 
